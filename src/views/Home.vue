4384e07e83f6799e988bb08d0872f61779ba5886
unknown <flt853568226@gmail.com>
2021-01-10 19:37:01 +0800

    first upload

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Addtodo/>
+    <Todos/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Addtodo from '../components/Addtodo'
+import Todos from '../components/Todos'
+ 
+export default {
+  name: 'Home',
+  components: {
+    Addtodo,
+    Todos
+  }
+}
+</script>
